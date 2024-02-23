52a6be2a89520c1a6b305b4a53c941c117e65a8e
Rodrigo Mendoza <rodrigo.dev29@yahoo.com>
2024-02-23 11:10:28 -0800

    updated script js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-const colors = ['red','pink', 'green', 'blue', 'yellow', 'orange','purple','dodgerblue','white'];
+const colors = ['red','pink', 'green', 'blue','limegreen', 'yellow', 'orange','purple','dodgerblue','white'];
 const SQUARES = 500;
 
 for (let i = 0; i < SQUARES; i++) {
